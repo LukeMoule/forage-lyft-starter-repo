2b6a749cb5e503e39c08e662695b1cbeb9b51b4d
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-05-07 14:34:47 +0100

    updated argument name to be consistent with class diagram

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -4,11 +4,11 @@ from engine import Engine
 
 
 class SternmanEngine(Engine, ABC):
-    def __init__(self, warning_light_is_on):
-        self.warning_light_is_on = warning_light_is_on
+    def __init__(self, warning_light_on):
+        self.warning_light_on = warning_light_on
 
     def needs_service(self):
-        if self.warning_light_is_on:
+        if self.warning_light_on:
             return True
         else:
             return False
